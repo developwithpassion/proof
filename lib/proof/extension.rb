e276db6cb85de3ad47bfdf317a414f3d740bca7a
Scott Bellware <sbellware@gmail.com>
2013-01-14 21:40:22 -0600

    Extension is encapsulated as an instance The design isn't clear Need to unwind the temporal couplings around mod method and mod.name

--- lib/proof/extension.rb
+++ lib/proof/extension.rb
@@ -1,27 +1,38 @@
 module Proof
-  module Extension
-    extend self
+  class Extension
+    attr_accessor :obj_under_test
 
-    def into(obj_under_test)
-      mod = (obj_under_test.class == Module ? obj_under_test : obj_under_test.class)
+    def self.into(obj_under_test)
+      instance = new
+      instance.obj_under_test = obj_under_test
+      instance.extend_obj_under_test
+    end
 
-      extension = proof_module(mod)
+    def mod
+      (obj_under_test.class == Module ? obj_under_test : obj_under_test.class)
+    end
+
+    def extend_obj_under_test
+      mod = self.mod
+      extension = proof_module
 
       if extension.nil?
-        Output.debug "#{mod.name} not extended by #{extension_name(mod)}"
+        Output.debug "#{mod.name} not extended by #{extension_name}"
         return false
       end
 
       if obj_under_test.is_a? extension
-        Output.debug "#{mod.name} is already extended by #{extension_name(mod)}"
+        Output.debug "#{mod.name} is already extended by #{extension_name}"
         return false
       end
 
-      Output.debug "Extending #{extension_name(mod)} into #{mod.name}"
+      Output.debug "Extending #{extension_name} into #{mod.name}"
       obj_under_test.extend extension
     end
 
-    def proof_module(mod)
+    def proof_module
+      mod = self.mod
+
       defined = mod.const_defined? :Proof, search_ancestors=false
       Output.debug "#{mod.name} has #{defined ? '' : 'no '}inner Proof"
 
@@ -31,7 +42,7 @@ module Proof
       mod.const_get :Proof, search_ancestors=false
     end
 
-    def extension_name(mod)
+    def extension_name
       "#{mod.name}::Proof"
     end
   end
