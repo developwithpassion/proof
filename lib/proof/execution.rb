b8cdedfa603243315ee5faaff7deff0427f30c78
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-01-15 12:19:36 -0600

    Added the write functionality back into the Result

--- lib/proof/execution.rb
+++ lib/proof/execution.rb
@@ -18,20 +18,10 @@ module Proof
       begin
         result = obj_under_test.instance_eval &blk
         status = result ? :pass : :fail
-        messsage = Proof::Description.current
       rescue => error
         status = :error
-        # backtrace = error.backtrace
-        # line_detail = backtrace[0].gsub(/.*\/proofs\/proof\/(.*\.rb.*)/,'\1')
-        # messsage = "(#{error.class}) \"#{error.message}\" at #{line_detail}"
       end
       Proof::Result.new status, error
-      # Output.send method, messsage
-
-      # if method == :error
-      #   backtrace.reject! {|l| l =~ /proof\/lib\/proof/ }
-      #   Output.details backtrace.join("\n")
-      # end
     end
   end
 end
--- lib/proof/result.rb
+++ lib/proof/result.rb
@@ -1,11 +1,23 @@
 module Proof
   class Result
+    attr_reader :status,:error
     def initialize(status, error = nil)
       @status = status
       @error = error
     end
 
     def write
+      Output.send status, "#{Description.current}"
+
+
+      if status == :error
+        backtrace = error.backtrace
+        line_detail = backtrace[0].gsub(/.*\/proofs\/proof\/(.*\.rb.*)/,'\1')
+        messsage = "(#{error.class}) \"#{error.message}\" at #{line_detail}"
+
+        backtrace.reject! {|l| l =~ /proof\/lib\/proof/ }
+        Output.details backtrace.join("\n")
+      end
     end
   end
 end
