7371112d0908e6a24f83f4a146f16739f87fe848
Scott Bellware <sbellware@gmail.com>
2013-01-15 23:30:13 -0600

    Output levels allow better control Logger configuration is less cumbersome Appenders are being use properly (no more appender-per-class)

--- proofs/proof/output.rb
+++ proofs/proof/output.rb
@@ -1,8 +1,10 @@
 require_relative '../proofs_init'
 
+# TODO turn on all loggers
 Proof::Output.info 'This informs'
 Proof::Output.pass 'This passes'
 Proof::Output.fail 'This fails'
 Proof::Output.error 'This is an error'
 Proof::Output.backtrace 'This is a backtrace'
 Proof::Output.details 'This is details'
+# TODO reset logger levels to default
