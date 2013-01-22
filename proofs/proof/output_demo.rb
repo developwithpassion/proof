2a78a13f54887e64c899d760aae3d09d7875aa38
Scott Bellware <sbellware@gmail.com>
2013-01-22 02:48:00 -0600

    Levels can be pushed and popped (push/pop demos need to be in output)

--- proofs/proof/output_demo.rb
+++ proofs/proof/output_demo.rb
@@ -7,9 +7,10 @@ Proof::Output.error 'This is an error. It prints at the warn level.'
 Proof::Output.backtrace 'This is a backtrace. It prints at the info error.'
 Proof::Output.details 'This is details, but it won\'t print at this level.'
 
-level = Proof::Output.level
-Proof::Output.level = :debug
-
+Proof::Output.push_level :debug
 Proof::Output.details 'This is details. It prints at the debug level.'
+Proof::Output.pop_level
 
-Proof::Output.level = level
+Proof::Output.push_level :debug do
+  Proof::Output.details 'This is details. It also prints at the debug level.'
+end
