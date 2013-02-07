2b904b1e69ea4dfe08b287d5bc2c24fa9ea5128f
Scott Bellware <sbellware@gmail.com>
2013-02-06 22:34:29 -0700

    Proofs for suite summary when no files in suite

--- lib/proof/runner/suite.rb
+++ lib/proof/runner/suite.rb
@@ -13,9 +13,17 @@ module Proof
         file_patterns = flatten(args)
         files = glob(file_patterns, base_dir)
 
+        if files.empty?
+          ::Proof::Output.summary "Suite has no files"
+          # return ExitCode.suite_has_no_files
+        end
+
         instance = new files
         results = instance.run
-        Summary.output(results)
+
+        # exit_code = Summary.output(results)
+
+        # return exit_code
       end
 
       def self.glob(patterns, base_dir)
@@ -36,7 +44,7 @@ module Proof
       end
 
       def run
-        appender = Output.push_device_from_opts(:string_io) do
+        appender = Output.push_device(:string_io) do
           files.each do |file|
             load file
           end
@@ -48,8 +56,8 @@ module Proof
       class Summary
         include Initializer
 
-        FAIL_PATTERN = /-> Fail:.*$/
         PASS_PATTERN = /Pass:.*$/
+        FAIL_PATTERN = /-> Fail:.*$/
         ERROR_PATTERN = /Error:.*$/
 
         initializer :results
@@ -72,13 +80,59 @@ module Proof
         end
 
         def output
-          summary = "Passed: #{passes.count}\n"
-          summary = "#{summary}Failed: #{fails.count}\n"
-          summary = "#{summary}Errors: #{errors.count}\n"
+          summary = ''
+          # summary = "Passed: #{passes.count}\n"
+          # summary = "#{summary}Failed: #{fails.count}\n"
+          # summary = "#{summary}Errors: #{errors.count}\n"
 
           Output.summary summary
+
+          # return exit_code
         end
+
+      #   def exit_code
+      #     ok = passes.count <= 0
+      #     failures = fails.count <= 0
+      #     errors = errors.count <= 0
+
+      #     return ExitCodes.errors if errors
+
+      #     return ExitCodes.failures if failures
+
+      #     return ExitCodes.ok if failures
+
+
+
+
+      #     if failures
+      #       return 0
+      #     elsif fatal
+      #       return 1
+      #     else
+      #       return -1
+      #     end
+      #   end
       end
+
+      # module ExitCodes
+      #   extend self
+
+      #   def ok
+      #     -1
+      #   end
+
+      #   def failures
+      #     0
+      #   end
+
+      #   def fatal
+      #     1
+      #   end
+
+      #   def suite_has_no_files
+      #     2
+      #   end
+      # end
     end
   end
 end
