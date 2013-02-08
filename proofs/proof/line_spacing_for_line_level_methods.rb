5f9077cb44ae1838f62f49c77cd3022de858440b
Scott Bellware <sbellware@gmail.com>
2013-02-08 01:49:55 -0700

    Simplified permutation test. Will use less general approach (but more compact)

--- proofs/proof/line_spacing_for_line_level_methods.rb
+++ proofs/proof/line_spacing_for_line_level_methods.rb
@@ -29,30 +29,27 @@ def line_level_methods
 end
 
 block do
-  combined = block_level_methods.product line_level_methods
-
-  combined.each do |combination|
+  block_level_methods.each do |block_level_method|
     otp = output
     dvc = device
-    otp.push_device dvc
 
-    block_level_method, line_level_method = combination
+    otp.push_device(dvc) do
+      heading "Pass Preceded By #{block_level_method.capitalize}" do
 
-    heading "#{line_level_method.capitalize} Preceded By #{block_level_method.capitalize}" do
+        some_block = "some #{block_level_method}"
+        some_line = "some pass"
 
-      block = "some #{block_level_method}"
-      line = "some #{line_level_method}"
+        otp.suspend_devices :stdout do
+          otp.write block_level_method, some_block
+          otp.pass some_line
+        end
 
-      otp.suspend_devices :stdout do
-        otp.write block_level_method, block
-        otp.write line_level_method, line
-      end
+        text = dvc.read
 
-      text = dvc.read
-
-      proof "#{line_level_method} is preceded by two new lines" do
-        line = "#{line_level_method.capitalize}: #{line}"
-        text.prove { line_level_preceded_by_two_new_lines? line, block }
+        proof "Pass is preceded by two new lines" do
+          line = "Pass: #{some_line}"
+          text.prove { line_level_preceded_by_two_new_lines? line, block }
+        end
       end
     end
   end
